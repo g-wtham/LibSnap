1bad96df4f9b862cb5c1b5543eafd4b65284b434
Gowtham <curiousgowtham@gmail.com>
2025-02-26 15:01:09 +0530

    send email

--- email_alert.py
+++ email_alert.py
@@ -17,3 +17,18 @@ def get_email(conn, roll_no):
     conn.close()
     if result:
         return result[0]
+
+def send_mail(to_email, subject, text):
+    if to_email is None:
+        return
+
+    msg = MIMEText(text, 'plain')
+    msg["From"] = username
+    msg['To'] = to_email
+    msg["Subject"] = subject    
+
+    with smtplib.SMTP(host="smtp.gmail.com", port=587) as server:
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(from_addr=username, to_addrs=to_email, msg=msg.as_string())
+        print(f"Email sent successfully to {to_email}")
